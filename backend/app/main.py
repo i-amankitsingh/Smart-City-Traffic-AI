397f6bdfa8fd78a6ce3b78718b141d4babfdf2c5
Ankit Singh <rixronger@gmail.com>
2026-02-02 23:25:27 +0530

    Initial commit

---
+++ backend/app/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI(title="AI Traffic Backend")
+
+@app.get("/")
+def index():
+    return {"message": "Hello, your Smart City Traffic AI API working fine!"}
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
